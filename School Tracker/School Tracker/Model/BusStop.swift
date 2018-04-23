4870ae029dbe55e36471ac33e5c3822fa8e651c5
Haurimton Andres Martin Franco <haurimton@iamat.com>
2018-04-23 11:13:13 -0500

    [Model] Create data models for SchoolBus and BusStop

---
+++ School Tracker/School Tracker/Model/BusStop.swift
@@ -0,0 +1,9 @@
+//
+//  BusStop.swift
+//  School Tracker
+//
+//  Created by Haurimton Andres Martin Franco on 4/23/18.
+//  Copyright © 2018 Haurimton Andres Martin Franco. All rights reserved.
+//
+
+import Foundation
