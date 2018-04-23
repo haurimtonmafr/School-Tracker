ae8121843dfee2caa5ae300e6e864df824b27b7f
Haurimton Andres Martin Franco <haurimton@iamat.com>
2018-04-23 12:51:25 -0500

    [BusStop] Create bus stop response model

--- School Tracker/School Tracker/Model/BusStop.swift
+++ School Tracker/School Tracker/Model/BusStop.swift
@@ -23,3 +23,23 @@ struct BusStop: Codable {
         case long = "lng"
     }
 }
+
+/**
+ Create model for BusStopResponse and conform Codable protocol in order to use Codabel JSON Parser
+ */
+struct BusStopResponse: Codable {
+    let response: Bool?
+    let busStops: [BusStop]?
+    let estimatedTime: Double?
+    let retryTime: Double?
+    
+    /**
+     Solve camelcase constants conflict in Swift
+     */
+    private enum CodingKeys: String, CodingKey {
+        case response
+        case busStops = "stops"
+        case estimatedTime = "estimated_time_milliseconds"
+        case retryTime = "retry_time_milliseconds"
+    }
+}
