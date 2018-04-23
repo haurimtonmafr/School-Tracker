58441b380a57906680959699061560655415ee1f
Haurimton Andres Martin Franco <haurimton@iamat.com>
2018-04-23 10:58:57 -0500

    [Init] Create Xcode project

---
+++ School Tracker/School Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  School Tracker
+//
+//  Created by Haurimton Andres Martin Franco on 4/23/18.
+//  Copyright © 2018 Haurimton Andres Martin Franco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
