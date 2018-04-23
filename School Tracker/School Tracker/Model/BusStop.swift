2826d1754e228511d652bdf5a186fe9d1c395a64
Haurimton Andres Martin Franco <haurimton@iamat.com>
2018-04-23 11:14:37 -0500

    [BusStop] Conform Codable protocol and add properties with CodingKeys

--- School Tracker/School Tracker/Model/BusStop.swift
+++ School Tracker/School Tracker/Model/BusStop.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+/**
+ Conform Codable protocol in order to use Codabel JSON Parser
+ */
+struct BusStop: Codable {
+    let lat: Double?
+    let long: Double?
+    
+    /**
+     Solve camelcase constants conflict in Swift
+     */
+    private enum CodingKeys: String, CodingKey {
+        case lat
+        case long = "lng"
+    }
+}
