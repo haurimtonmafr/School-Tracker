754ad33cf8a3d3220afe853fe710f7e6da32a13a
Haurimton Andres Martin Franco <haurimton@iamat.com>
2018-04-24 10:25:28 -0500

    [GoogleMaps] Setup framework

--- School Tracker/School Tracker/AppDelegate.swift
+++ School Tracker/School Tracker/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyCb3VZ4Bqi9nVX9tuRsgmShuXOVndtJK18")
+        
         return true
     }
 
@@ -27,6 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        NetworkReachability.shared.stopMonitoring()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -35,6 +38,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        NetworkReachability.shared.startMonitoring()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
